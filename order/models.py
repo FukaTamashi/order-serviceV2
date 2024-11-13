1671fb734a6dec4477b6901b8c55d84641f42e8e
FukaTamashi <vstepaniuk1@mail.ru>
2024-11-13 15:43:31 +0300

    feat: added meta class to rename table user and order

--- order/models.py
+++ order/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from user.models import User
-from user.validators import validate_order_name
+from store.validators import validate_order_name
 
 
 class Order(models.Model):
+    class Meta:
+        db_table = 'order'
+
     name = models.CharField(
         max_length=100,
         validators=[validate_order_name]
     )
     description = models.TextField()
-    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'order'
+    user = models.ForeignKey(
+        User,
+        related_name='orders',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
--- user/models.py
+++ user/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from user.validators import validate_age, validate_name
+from store.validators import validate_age, validate_name
 
 
 class User(models.Model):
+    class Meta:
+        db_table = 'user'
+
     name = models.CharField(
         max_length=100,
         validators=[validate_name]
@@ -11,9 +14,6 @@ class User(models.Model):
     age = models.IntegerField(
         validators=[validate_age]
     )
-
-    class Meta:
-        db_table = 'user'
 
     def __str__(self):
         return self.name
